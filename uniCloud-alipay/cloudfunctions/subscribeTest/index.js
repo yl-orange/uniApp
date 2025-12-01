edbe217c02a9fcb0f77c08da425472a66d0dd376
cheng <2212892867@qq.com>
2025-12-01 18:23:50 +0800

    常规提交

--- uniCloud-alipay/cloudfunctions/subscribeTest/index.js
+++ uniCloud-alipay/cloudfunctions/subscribeTest/index.js
@@ -1,39 +1,23 @@
 'use strict';
-// 引入uni-subscribemsg公共模块
 const UniSubscribemsg = require('uni-subscribemsg');
 
 exports.main = async (event, context) => {
-  // 初始化实例
   let uniSubscribemsg = new UniSubscribemsg({
-  	dcloudAppid: "__UNI__B62FF0C",
-  	provider: "weixin-h5", // 注意，这里是weixin-h5
+    dcloudAppid: "__UNI__B62FF0C",
+    provider: "weixin-mp", // ✅ 小程序订阅消息用 weixin-mp
   });
-  // 发送模板消息
-  let res = await uniSubscribemsg.sendTemplateMessage({
-  	touser: "",
-  	template_id: "WhDz7V9NVqZi6Fo3K0-CB7TJuOlU21LhOlo9YaoXsc8",
-  	url: "https://uniapp.dcloud.net.cn", // 用户点击消息后跳转的链接地址
-  	data: {
-  		first: {
-  			value: "您购买的套餐已到期!",
-  			color: "#666666"
-  		},
-  		keyword1: {
-  			value: "test@qq.com",
-  			color: "#666666"
-  		},
-  		keyword2: {
-  			value: "阿里云空间",
-  			color: "#666666"
-  		},
-  		keyword3: {
-  			value: "2023-12-21 15:30:20",
-  			color: "#666666"
-  		},
-  		remark: {
-  			value: "请及时续费",
-  			color: "#666666"
-  		}
-  	}
+
+  let res = await uniSubscribemsg.sendSubscribeMessage({
+    touser: "", // 小程序用户的 openid
+    template_id: "WhDz7V9NVqZi6Fo3K0-CB7TJuOlU21LhOlo9YaoXsc8",
+    page: "pages/index/index", // 小程序内页面
+    miniprogram_state: "formal", // developer/trial/formal
+    lang: "zh_CN",
+    data: {
+      // key 名要和订阅模板里的字段对上，比如 name1、time2 之类
+      name1: { value: "张三" },
+      time2: { value: "2023-12-21 15:30:20" },
+      // ...
+    }
   });
 };
